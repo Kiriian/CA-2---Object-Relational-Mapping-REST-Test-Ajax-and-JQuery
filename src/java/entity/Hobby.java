2360c8f344533e5109aa5f03d628292c1375b1f1
Jeanette <kiriian@hotmail.com>
2015-10-05 11:40:39 +0200

    har lavet person og hobby entity klasserne. har også lavet mange til mange forhold i mellem dem.

---
+++ src/java/entity/Hobby.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.util.ArrayList;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+/**
+ *
+ * @author Jeanette
+ */
+@Entity
+public class Hobby
+{
+
+    @Id
+    private long id;
+    private String name;
+    private String description;
+    @ManyToMany(mappedBy = "hobbyList")
+    private ArrayList<Person> personList = new ArrayList();
+
+    public Hobby()
+    {
+    }
+
+    public Hobby(long id, String name, String description)
+    {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public ArrayList<Person> getPersonList()
+    {
+        return personList;
+    }
+
+    public void setPersonList(ArrayList<Person> personList)
+    {
+        this.personList = personList;
+    }
+    
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+}
---
+++ src/java/entity/Person.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.util.ArrayList;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+/**
+ *
+ * @author Jeanette
+ */
+@Entity
+public class Person
+{
+    @Id
+    private long id;
+    private String firstName;
+    private String lastName;
+    @ManyToMany
+    private ArrayList<Hobby> hobbyList = new ArrayList();
+
+    public Person()
+    {
+    }
+
+    public Person(long id, String firstName, String lastName)
+    {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+   
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Hobby> getHobbyList()
+    {
+        return hobbyList;
+    }
+
+    public void setHobbyList(ArrayList<Hobby> hobbyList)
+    {
+        this.hobbyList = hobbyList;
+    }
+    
+    
+}
